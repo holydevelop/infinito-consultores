7b278496009ec769c6d3c290b4adb950f873860f
holydevelop <101340405+holydevelop@users.noreply.github.com>
2023-11-16 13:48:15 -0300

    Historial errores
    Ahora si funciona jiji

--- src/app/historial/page.tsx
+++ src/app/historial/page.tsx
@@ -4,15 +4,18 @@ import Navbar from "@/components/Navbar";
 
 import { useAppSelector } from "@/redux/hooks"
 import { GetHistorial } from "@/utils/api"
+import { Job } from "@/utils/job";
 import React, { useEffect } from "react"
 
 export default function Historial() {
-  const [jobs, setJobs]: [any | null, any] = React.useState<any | null>(null);
+  const [userRes, setUserRes] = React.useState(null)
+  const [jobs, setJobs]: [any[] | null, any] = React.useState<any[] | null>(null);
   const user = useAppSelector(state => state.user)
 
   async function loadJobs() {
     try {
-      const { data } = await GetHistorial(user?.id)
+      const {data} = await GetHistorial(user?.id)
+      console.log("Data: ",data)
       setJobs(data)
     } catch (err) {
       console.error(err)
@@ -26,10 +29,12 @@ export default function Historial() {
     }
   }, [user])
 
+  // Este efecto se dispara solo cuando `jobs` cambia
+  useEffect(() => {}, [jobs]);
+
   return (
     <div>
       <Navbar />
-
       {
         jobs ?
           <JobList jobs={jobs} />
